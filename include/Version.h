038cc20ad9734e22fb6be77e6199197ec5315897
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2018-02-11 12:03:04 -0500

    Remove ambiguity from evil preprocessor macros

--- include/Version.h
+++ include/Version.h
@@ -12,6 +12,6 @@
 #include <string>
 
 // See CHANGELOG for a list of what has changed between versions
-static const std::string LinthesiaVersionString = VERSION;
+static const std::string LinthesiaVersionString = CMAKE_PROJECT_VERSION;
 
 #endif // __LINTHESIA_VERSION_H__
